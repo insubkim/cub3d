fa10a3238fd5689ff48f858c4704ada54e02c215
MoongMoongBook <j.j.kant1948@gmail.com>
2023-07-16 11:43:23 +0900

    refactor : 주석 + 테스트 함수 구분

--- sources/loop.c
+++ sources/loop.c
@@ -6,7 +6,7 @@
 /*   By: insub <insub@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 19:18:56 by inskim            #+#    #+#             */
-/*   Updated: 2023/07/16 11:34:32 by insub            ###   ########.fr       */
+/*   Updated: 2023/07/16 11:41:00 by insub            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ void	move_dir(enum e_direction dir, t_player *player){
 }
 
 /*
-	atans(기울기) -> 라디안임.
+	atan2(기울기) -> 라디안임.
 	라디안을 알면 sin cos를 이용하여 x,y 구할 수 있음.
 	up down 은 방향 벡터로, left right은 카메라 벡터로(방향벡터와 수직이므로)
 	down, left의 경우 반대로 가야하니 음수처리.
@@ -55,13 +55,13 @@ void	move_player(enum e_direction dir, t_game *game_info){
 	double	x;
 	double	y;
 
-	if (dir == UP || dir == DOWN)//dir 방향으로 
+	if (dir == UP || dir == DOWN)//dir 방향일때,
 		radian = atan2(game_info->player.dir.y, game_info->player.dir.x);
-	else //if (dir == LEFT || dir == RIGHT)//plane 방향으로 
+	else //plane 방향일때,
 		radian = atan2(game_info->player.plane.y, game_info->player.plane.x);
 	x = cos(radian) * ((double)1 / TILESIZE);
 	y = sin(radian) * ((double)1 / TILESIZE);
-	if (dir == DOWN || dir == LEFT)
+	if (dir == DOWN || dir == LEFT)// down 이나 left면  방향 반대이므로 -1 곱함.
 	{
 		x *= -1;
 		y *= -1;
@@ -76,7 +76,15 @@ void	move_player(enum e_direction dir, t_game *game_info){
 	}
 }
 
-#include <stdio.h>//printf용. 지워야함.
+
+//test용 loop 값 확인
+#include <stdio.h>
+void	__test_print(t_game *game_info){
+	printf("loc\t%lf    %lf\n", game_info->player.loc.x, game_info->player.loc.y);
+	printf("dir\t%lf    %lf\n", game_info->player.dir.x, game_info->player.dir.y);
+	printf("plane\t%lf    %lf\n\n", game_info->player.plane.x, game_info->player.plane.y);	
+}
+
 int	handle_key(int keycode, t_game *game_info){
 	if (keycode == KEY_LEFT)
 		move_dir(LEFT, &game_info->player);
@@ -92,14 +100,21 @@ int	handle_key(int keycode, t_game *game_info){
 		move_player(RIGHT, game_info);
 	else if (keycode == KEY_ESC)
 		handle_close(game_info);
-	//---------test
-	printf("loc\t%lf    %lf\n", game_info->player.loc.x, game_info->player.loc.y);
-	printf("dir\t%lf    %lf\n", game_info->player.dir.x, game_info->player.dir.y);
-	printf("plane\t%lf    %lf\n\n", game_info->player.plane.x, game_info->player.plane.y);	
-	//-------------
-	
 	//raycasting + 변경 된 화면에 표시, 이전 이미지 파괴.
+	__test_print(game_info);
 	return (0);
+}
+
+//test 용
+void	__test_init(t_game *game_info){
+	game_info->map.height = 20;
+	game_info->map.width = 20;
+	game_info->player.loc.x = 10;
+	game_info->player.loc.y = 10;
+	game_info->player.dir.x = -1;
+	game_info->player.dir.y = 0;
+	game_info->player.plane.x = 0;
+	game_info->player.plane.y = 0.66;
 }
 
 int	main(void){
@@ -107,26 +122,13 @@ int	main(void){
 	game_info.mlx = mlx_init();
 	game_info.win = mlx_new_window(game_info.mlx, WIN_WIDTH, WIN_HEIGHT, "Cub3d");
 	
-	
-	//-------test
-	game_info.map.height = 20;
-	game_info.map.width = 20;
-	game_info.player.loc.x = 10;
-	game_info.player.loc.y = 10;
-	game_info.player.dir.x = -1;
-	game_info.player.dir.y = 0;
-	game_info.player.plane.x = 0;
-	game_info.player.plane.y = 0.66;
-	//-----------
+	__test_init(&game_info);
+	//parse + set t_game
 
-	//parse + init t_game
-
-	//raycasting + 첫 화면에 표시
+	//raycasting + 첫 화면 표시
 
 	//hook
-	//어떤 키 입력이 들어오면 플레이어 위치를 변경시키고 화면에 표시.
 	mlx_hook(game_info.win, 2, 0, handle_key, &game_info);
-	//리소스 해제
 	mlx_hook(game_info.win, 17, 0, handle_close, &game_info);
 	mlx_loop(game_info.mlx);
 	//리소스 해제
