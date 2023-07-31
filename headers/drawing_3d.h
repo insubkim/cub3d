cda5712980f72d47e058c2bf1c1368d9f2429c98
Heeoh Son <heson@cx2r5s5.42seoul.kr>
2023-07-31 18:25:56 +0900

    style: norm done

--- headers/drawing_3d.h
+++ headers/drawing_3d.h
@@ -6,7 +6,7 @@
 /*   By: heson <heson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 01:17:45 by heson             #+#    #+#             */
-/*   Updated: 2023/07/31 18:17:34 by heson            ###   ########.fr       */
+/*   Updated: 2023/07/31 18:25:35 by heson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,23 +51,25 @@ typedef struct s_drawing_line_data {
 }	t_drawing_line_data;
 
 // raycasting
-void	init_side_data_of_ray(t_side_data_of_ray *ray, int ray_loc,
-			double ray_dir, double player_loc);
-void	init_vars_for_raycasting(t_ray_data *ray, t_player player,
-			double camera_x);
-double	get_dist_of_ray(t_ray_data *ray, char **map_board);
+void			init_side_data_of_ray(t_side_data_of_ray *ray, int ray_loc,
+					double ray_dir, double player_loc);
+void			init_vars_for_raycasting(t_ray_data *ray, t_player player,
+					double camera_x);
+double			get_dist_of_ray(t_ray_data *ray, char **map_board);
 
 // wall_drawing
-void	init_vars_for_drawing_line(t_drawing_line_data *data,
-			double dist_of_ray, int x);
-void	init_texture_data_for_drawing_line(t_drawing_line_data *data,
-			double dist_of_ray, t_ray_data ray, t_game game_info);
-void	draw_line(t_drawing_line_data data, t_img *window_img);
-void    draw_map(t_player player, t_img img, t_map map);
+void			init_vars_for_drawing_line(t_drawing_line_data *data,
+					double dist_of_ray, int x);
+void			init_texture_data_for_drawing_line(t_drawing_line_data *data,
+					double dist_of_ray, t_ray_data ray, t_game game_info);
+void			draw_line(t_drawing_line_data data, t_img *window_img);
+void			draw_map(t_player player, t_img img, t_map map);
 
 // drawing_uils
-int				get_texture_x(t_vector player_loc, t_ray_data ray, double dist, int texture_width);
-t_img			get_texture_img(int hit_side, t_point hitpoint, t_map map, t_vector player_loc);
+int				get_texture_x(t_vector player_loc, t_ray_data ray,
+					double dist, int texture_width);
+t_img			get_texture_img(int hit_side, t_point hitpoint,
+					t_map map, t_vector player_loc);
 unsigned int	get_color(t_img img, int x, int y);
 void			my_mlx_pixel_put(t_img *img, int x, int y, int color);
 
